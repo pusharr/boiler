1328a1a7807d392d80d4d23c597fef99e19b8f51
Geezar <nirozaz@yahoo.com>
2021-03-20 22:15:16 +0200

    first commit

--- client/config/root.js
+++ client/config/root.js
@@ -8,7 +8,7 @@ import { Switch, Route, Redirect, StaticRouter } from 'react-router-dom'
 import store, { history } from '../redux'
 
 import Home from '../components/home'
-import DummyView from '../components/dummy-view'
+import Game from '../components/Game'
 import NotFound from '../components/404'
 
 import Startup from './startup'
@@ -72,9 +72,9 @@ const RootComponent = (props) => {
       <RouterSelector history={history} location={props.location} context={props.context}>
         <Startup>
           <Switch>
-            <Route exact path="/" component={() => <DummyView />} />
+            <Route exact path="/" component={() => <Game />} />
             <Route exact path="/dashboard" component={() => <Home />} />
-            <PrivateRoute exact path="/hidden-route" component={() => <DummyView />} />
+            <PrivateRoute exact path="/hidden-route" component={() => <Game />} />
             <Route component={() => <NotFound />} />
           </Switch>
         </Startup>
